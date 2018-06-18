503aaa83ba7d5c678f8922058ad70011598640d0
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-06-17 17:36:25 -0700

    carls help

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,34 +6,51 @@ var questions = [{question:"What was the most popular video game in 2009", answe
 //2. Chooses Answers
 //3. Resets the game
 $(document).on('click', '.start', function(e) {
-   game.displayQuestions(e); 
+   game.start(e); 
    });
 
 //Creates the logic to push to the document.onclick
 var game = {
     displayQuestions:function() {
-        $(".gameContainer").html("<h1>" + "TEST ME" + "</h1>")
+        $('.gameContainer').html('<h1>' + 'TEST ME' + '</h1>')
     },
+
 
     counter:120,
     countdown:function() {
-        game.counter--;
-        $('#counter-timer').html(game.counter);
-
-        if (game.counter === 0) {
-            console.log("TIME IS UP YOU FOOL");
-            game.done();
-        }
+        setInterval(function() {
+            game.counter--;
+            console.log(game.counter)
+            $('#counter-timer').html(game.counter);
+            if (game.counter === 0) {
+                console.log('TIME IS UP YOU FOOL');
+                game.done();
+            }
+        }, 1000)
+      
     },
+
+    score:0,
+    
 
     start:function() {
-        timer=setInterval(game.countdown, 1000);
+       this.countdown();
+        $('.scoreContainer').html('<h1>' + score + '</h1>');
+        
+        //add time remaining to the dom - DISPLAY IT!
+        
+        //for loop questions to dipslay on DOM using append
+        //radio buttons included in answers - make logic to add radio buttons
+        //Creates a button that lets the game know you are finished, so a function to end the game if they click a button or run out of time
     },
 
-    done:function() {
+    //done function when the timer hits zero or when you click the done button
+    //checks your answers
+    //resets the game
+    //displays the final score
+   /*  done:function() {
         if 
-    }
+    }  */
 
 };
-
 
